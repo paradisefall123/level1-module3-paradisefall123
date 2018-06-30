f7ad508da04f4a469c7aafa0e5793a148f277386
J.D.S. Solutions <zaelix@gmail.com>
2018-06-30 11:21:56 -0700

    Formatted & Updated Copyright

--- src/CowTimer.java
+++ src/CowTimer.java
@@ -1,9 +1,7 @@
 /*
- *    Copyright (c) The League of Amazing Programmers 2013-2017
+ *    Copyright (c) The League of Amazing Programmers 2013-2019
  *    Level 1
  */
-
-
 
 import java.applet.AudioClip;
 import java.io.IOException;
@@ -11,9 +9,8 @@ import javax.swing.JApplet;
 
 public class CowTimer {
 	/*
-	 * This is an advanced recipe. There may be more than one line of code for
-	 * each instruction.
-	 * Work in seconds when testing, then change to minutes
+	 * This is an advanced recipe. There may be more than one line of code for each
+	 * instruction. Work in seconds when testing, then change to minutes
 	 */
 
 	public static void main(String[] args) throws InterruptedException {
@@ -30,25 +27,24 @@ public class CowTimer {
 
 	public void start() throws InterruptedException {
 		/*
-		 * 2. Count down the minutes, print the current minute then sleep for 60
-		 * seconds using Thread.sleep(int milliseconds).
+		 * 2. Count down the minutes, print the current minute then sleep for 60 seconds
+		 * using Thread.sleep(int milliseconds).
 		 */
 
 		/*
-		 * 3. When the timer is finished, use the playSound method to play a moo
-		 * sound. You can use the .wav file in the default package, or you can download 
-		 * one from freesound.org, then drag it intothe default package. 
+		 * 3. When the timer is finished, use the playSound method to play a moo sound.
+		 * You can use the .wav file in the default package, or you can download one
+		 * from freesound.org, then drag it intothe default package.
 		 */
 
 	}
 
 	private void playSound(String fileName) {
-		AudioClip sound = JApplet
-				.newAudioClip(getClass().getResource(fileName));
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
 		sound.play();
 	}
 
-private void speak(String stuffToSay) {
+	private void speak(String stuffToSay) {
 		try {
 			Runtime.getRuntime().exec("say " + stuffToSay).waitFor();
 		} catch (Exception e) {
@@ -56,6 +52,4 @@ private void speak(String stuffToSay) {
 		}
 	}
 
-
 }
-
