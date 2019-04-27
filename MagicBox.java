c30a1265a671d644025272526643f78de0b137be
paradisefall123 <paradisewendy123@gmail.com>
2019-04-27 11:26:57 -0700

    I need to finish magic box.

---
+++ MagicBox.java
@@ -0,0 +1,133 @@
+/*
+ *    Copyright (c) The League of Amazing Programmers 2013-2018
+ *    Level 1
+ */
+
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class MagicBox extends JPanel implements Runnable, MouseListener {
+		
+	/*
+	 * We are going to hide secrets within the magic box. 
+	 * When the user clicks on a secret place, stuff will happen.
+	 * 
+	 * 1. Make the frame respond to mouse clicks.
+	   
+	 * 2. When the mouse is clicked, use the Media Palace (read the code in the default package) to play sounds, show images or speak.
+	 * 
+	 * 3. backgroundImage.getRGB(keyEvent.getX(), keyEvent.getY()) will give you the color of the current pixel.
+	 */
+	
+	JFrame frame = new JFrame("The Magic Box contains many secrets...");
+
+	BufferedImage backgroundImage;
+
+	public static void main(String[] args) throws Exception {
+		SwingUtilities.invokeLater(new MagicBox());
+	
+				
+	}
+
+	@Override
+	public void run() {
+		try {
+			loadBackgroundImage();
+			createUI();
+		} catch (Exception w) {
+			System.err.println(w.getMessage());
+		}
+	}
+
+	private void createUI() {
+		
+		frame.add(this);
+		setPreferredSize(new Dimension(backgroundImage.getWidth(), backgroundImage.getHeight()));
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.addMouseListener(this);
+	
+	}
+
+	private void loadBackgroundImage() throws Exception {
+		String imageFile = "src/magic-box.jpg";
+		try {
+			backgroundImage = ImageIO.read(new File(imageFile));
+		} catch (IOException e) { 
+			throw new Exception("Could not load image: " + imageFile);
+		}
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		g.drawImage(backgroundImage, 0, 0, null);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		int mouseY = e.getY();
+		int mouseX = e.getX();
+		System.out.println("mouse clicked");
+		System.out.println("mouse x: " + e.getX());
+		System.out.println("mouse y:" + e.getY());
+		if(mouseX>=335 && mouseX<=363 && mouseY>=552  && mouseY<=567) {			
+			JFrame two=new JFrame();
+			two.setVisible(true);
+			MediaPalace mp=new MediaPalace();
+			JLabel jl=mp.loadImageFromWithinProject("maxresdefault.jpg");
+			two.add(jl);
+			two.pack();	
+		    
+		} else if(mouseX>=428 && mouseX<=687 && mouseY>=702 && mouseY<=732) {
+			JFrame three=new JFrame();
+			three.setVisible(true);
+			MediaPalace pm=new MediaPalace();
+			JLabel lj=pm.loadImageFromWithinProject("blue-java-bananas.jpg");
+			three.add(lj);
+			three.pack();
+		}
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
+
