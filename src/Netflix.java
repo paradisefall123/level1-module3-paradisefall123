2d538e704c54d08050ce97b9a4592ef1053aa3ea
Your Name <you@example.com>
2019-05-04 11:28:38 -0700

    finish netflix

--- src/Netflix.java
+++ src/Netflix.java
@@ -1,4 +1,12 @@
 
 public class Netflix {
-
+		
+	
+	public static void main(String[] args) {
+		Movie ForrestGump=new Movie(); 
+		Movie Shrek=new Movie();
+		Movie MyNeighborTotoro=new Movie();
+		Movie CorpseBribe= new Movie();
+		Movie 
+	}
 }
